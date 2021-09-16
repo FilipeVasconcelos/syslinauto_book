aedef38de7650f1cc4ba9f215281626e0cf2df5d
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2021-09-16 17:23:57 +0200

    wv

--- animation/2nd_bode/gen.py
+++ animation/2nd_bode/gen.py
@@ -12,7 +12,7 @@ for xi in np.arange(0.1,1.0,0.01):
     gain=1    
     num=lambda p : 1
     den=lambda p : 1+2*xi*p+p**2
-    H_bo=Ftransfert(num=num,den=den,gain=gain,name="$\\xi="+f'{xi:.2f}$',DPI=200,verbeux=0)
+    H_bo=Ftransfert(num=num,den=den,gain=gain,name="$\\xi="+f'{xi:.2f}$',DPI=250,verbeux=0)
     fig=H_bo.bode(n=1024,y1lim=(-80,20),y2lim=(-180,0),color="tab:green",arrow_pcts=[0.3])
     #plt.title("$\\xi="+f'{xi:.2f}$',loc='left')
     fname='2nd_bode_files/2nd_bode_'+str(k)+'.png'
@@ -24,7 +24,7 @@ for xi in np.arange(1.0,0.1,-0.01):
     gain=1    
     num=lambda p : 1
     den=lambda p : 1+2*xi*p+p**2
-    H_bo=Ftransfert(num=num,den=den,gain=gain,name="$\\xi="+f'{xi:.2f}$',DPI=200,verbeux=0)
+    H_bo=Ftransfert(num=num,den=den,gain=gain,name="$\\xi="+f'{xi:.2f}$',DPI=250,verbeux=0)
     fig=H_bo.bode(n=1024,y1lim=(-80,20),y2lim=(-180,0),color="tab:green",arrow_pcts=[0.3])
     fname='2nd_bode_files/2nd_bode_'+str(k)+'.png'
     print(xi,fname)
