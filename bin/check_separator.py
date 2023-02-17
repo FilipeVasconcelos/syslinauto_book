50d17a0e129c904bb6927ff1943259ae48dba34b
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2023-02-17 12:14:12 +0100

    cleaning source

--- bin/check_separator.py
+++ bin/check_separator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 import argparse
-beginendenv=["figure","align","align*","center","table","bequation","itemize","criteria",
-            "theorem","definition","tcolorbox"]
+beginendenv=["figure","align","align*","center","table","bequation",
+             "itemize","criteria","theorem","definition","tcolorbox"]
 braketenv=["chapter","section","subsection","susubsection","paragraph",
          "exercice","question"]
 class bcolors:
